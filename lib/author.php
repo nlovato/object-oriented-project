ceb53a35f0f76431bb6766bf4a7ea7a3f8bbde1c
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-23 16:38:51 -0600

    Updated folders author.php/author.sql

--- lib/author.php
+++ lib/author.php
@@ -1,12 +1,4 @@
-create table author(
-authorId binary(16) not null,
-authorAvatarUrl varchar(255),
-authorActivationToken char(32),
-authorEmail varchar(128) not null,
-authorHash char(97) not null,
-authorUsername varchar(32) not null,
-unique(authorEmail),
-unique(authorUsername),
-INDEX(authorEmail),
-primary key(authorId)
-);
+<?php
+require_once ("Author.php");
+require_once(dirname(__DIR__,2) . "/vendor/autoload.php");
+$oVal = (object)[\nmarshloavto\ObjectOrientedProject\Author::class];
