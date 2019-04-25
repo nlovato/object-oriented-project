330612e36446928ff6dc408f85a5d818c4cc3b14
Natasha Lovato <nmarshlovato@cnm.edu>
2019-04-25 16:22:27 -0600

    updated UUID

--- php/classes/Author.php
+++ php/classes/Author.php
@@ -2,7 +2,7 @@
 
 namespace nmarshlovato\ObjectOrientedProject;
 
-require_once(dirname(__DIR__,2) . "/classes/autoload.php");
+require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 /**
--- php/lib/author.php
+++ php/lib/author.php
@@ -1,16 +1,16 @@
 <?php
 
-/**
+/*
  * setup require_once, namespaces (make sure to include both autoloaders
  * use the new keyword to call the constructor in the class and add all required parameters
  * vardump() the result from the step above
- **/
+ */
 require_once (dirname(__DIR__) . "/vendor/autoload.php");
-require_once(dirname(__DIR__,2) . "/classes/autoload.php");
+require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 
 use nmarshlovato\ObjectOrientedProject\Author;
 
-	$authorId = new Author("f805a6fb-83c8-4690-bcb2-19b170cfc8f8",
+	$authorId = new Author("23f0d40babd5456d8ae516186e135a01",
 	"avatar", "0abf9d115769410ab1d55e0acec5f8b0", "new@email.com",
 	'$argon2i$v=19$m=1024,t=384,p=2$T1B6Ymdqa3FJdmZqaDdqYg$hhyC1jf2WjbgfD8Jp6GZE9Tg3IpsYpXKm2VWYOJq8LA',
 	"newAuthorUsername");
